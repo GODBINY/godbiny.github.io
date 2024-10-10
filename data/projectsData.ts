e71735a9a79b95d5555380853709fe2f9264e381
godbin <arashi0121@naver.com>
2024-10-11 01:20:09 +0900

    add some files

---
+++ data/projectsData.ts
@@ -0,0 +1,23 @@
+interface Project {
+  title: string
+  description: string
+  href?: string
+  imgSrc?: string
+}
+
+const projectsData: Project[] = [
+  {
+    title: 'Preparing...',
+    description: `I will make some projects....:-)`,
+    imgSrc: '/static/images/prepare.avif',
+    href: 'https://www.google.com',
+  },
+  {
+    title: 'Preparing.....',
+    description: `I will make some projects....:-D`,
+    imgSrc: '/static/images/prepare.avif',
+    href: 'https://www.google.com',
+  },
+]
+
+export default projectsData
