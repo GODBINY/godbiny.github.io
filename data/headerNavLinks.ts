b420280261161138b0a9e7e2629f71657e7e423b
godbin <arashi0121@naver.com>
2024-11-03 23:13:30 +0900

    add: wiki page 추가

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -3,6 +3,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
+  { href: '/wiki', title: 'My Wiki' },
   { href: '/about', title: 'About' },
 ]
 
