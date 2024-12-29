35cc2a723785e83b9e7909f56ec01fb989b180d9
GODBINY <91115132+GODBINY@users.noreply.github.com>
2024-12-29 15:20:50 +0000

    Deploying to gh-pages from @ GODBINY/godbiny.github.io@64c5bc9167d92b9d6e5aa11a0da12d20dc3ccd5f 🚀

--- _next/static/chunks/app/not-found-21102a344b8b693d.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[160,124,904],{2077:function(n,e,u){Promise.resolve().then(u.t.bind(u,231,23))}},function(n){n.O(0,[231,971,23,744],function(){return n(n.s=2077)}),_N_E=n.O()}]);
